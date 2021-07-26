ab9318a9a26611fabd6d50deb842ea07dd73db5d
Peter Turner <peter.turner@hcl.com>
2021-07-26 23:09:44 +0100

    Added SumAll

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,4 +1,4 @@
-package main
+package arrays
 
 func Sum(numbers []int) int {
 	sum := 0
@@ -6,4 +6,15 @@ func Sum(numbers []int) int {
 		sum += number
 	}
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	lengthOfNumbers := len(numbersToSum)
+	sums= make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+	
+	return sums
 }
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,9 @@
-package main
+package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -12,6 +15,20 @@ func TestSum(t *testing.T) {
 
 		if got != want {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+}
+
+func TestSumAll(t *testing.T) {
+
+	t.Run("sum 2 slices", func(t *testing.T) {
+
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
 		}
 	})
 }
